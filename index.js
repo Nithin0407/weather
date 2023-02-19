4bba3725d3ff6b9b3f1d050919aecbd2d437a903
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-02-19 23:11:50 +0530

    commented code

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require('express')
-const app = express()
+// Require the Express package and create an Express application instance
+const express = require('express');
+const app = express(); //instance of app
 
+// Define a route for the root URL that sends "Hello World!" as the response
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
+// Start the server and listen for incoming requests on port 3000
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
